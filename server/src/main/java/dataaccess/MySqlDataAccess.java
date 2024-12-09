4001aeb6dbc8c7bb3ad10c1b66bc8b08abec09e3
vwj1776 <vwj1776@gmail.com>
2024-12-09 10:33:36 -0700

    chess, editing createStatements

---
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -0,0 +1,143 @@
+package dataaccess;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import model.Pet;
+import model.PetType;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+
+public class MySqlDataAccess implements DataAccess {
+
+    public MySqlDataAccess() throws ResponseException {
+        configureDatabase();
+    }
+
+    public Pet addPet(Pet pet) throws ResponseException {
+        var statement = "INSERT INTO pet (name, type, json) VALUES (?, ?, ?)";
+        var json = new Gson().toJson(pet);
+        var id = executeUpdate(statement, pet.name(), pet.type(), json);
+        return new Pet(id, pet.name(), pet.type());
+    }
+
+    public Pet getPet(int id) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT id, json FROM pet WHERE id=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, id);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readPet(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    public Collection<Pet> listPets() throws ResponseException {
+        var result = new ArrayList<Pet>();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT id, json FROM pet";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        result.add(readPet(rs));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return result;
+    }
+
+    public void deletePet(Integer id) throws ResponseException {
+        var statement = "DELETE FROM pet WHERE id=?";
+        executeUpdate(statement, id);
+    }
+
+    public void deleteAllPets() throws ResponseException {
+        var statement = "TRUNCATE pet";
+        executeUpdate(statement);
+    }
+
+    private Pet readPet(ResultSet rs) throws SQLException {
+        var id = rs.getInt("id");
+        var json = rs.getString("json");
+        var pet = new Gson().fromJson(json, Pet.class);
+        return pet.setId(id);
+    }
+
+    private int executeUpdate(String statement, Object... params) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+    CREATE TABLE IF NOT EXISTS AuthData (
+      `authToken` VARCHAR(36) PRIMARY KEY,
+      `username` VARCHAR(255) NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+    """,
+            """
+    CREATE TABLE IF NOT EXISTS GameData (
+      `gameID` INT PRIMARY KEY AUTO_INCREMENT,
+      `whiteUsername` VARCHAR(255) NOT NULL,
+      `blackUsername` VARCHAR(255) NOT NULL,
+      `gameName` VARCHAR(255) NOT NULL,
+      `game` BLOB NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+    """,
+            """
+    CREATE TABLE IF NOT EXISTS UserData (
+      `username` VARCHAR(255) PRIMARY KEY,
+      `password` VARCHAR(255) NOT NULL,
+      `email` VARCHAR(255) UNIQUE NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+    """
+    };
+
+
+    private void configureDatabase() throws ResponseException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+}
