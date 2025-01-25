52424803e9f2b7709b1db0ccc5a690b6d68795b1
vwj1776 <vwj1776@gmail.com>
2025-01-24 23:06:23 -0700

    added queen and rook because they are the same as bishop

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -1,13 +1,81 @@
 package chess;
 
-import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class QueenMovesCalculator {
-    public QueenMovesCalculator(ChessBoard board, ChessPosition myPosition) {
+
+    private ChessBoard board;
+    private ChessPosition position;
+    private Set<ChessMove> validMoves;
+
+    public QueenMovesCalculator(ChessBoard board, ChessPosition position) {
+        this.board = board;
+        this.position = position;
+        this.validMoves = new HashSet<>();
     }
 
-    public Collection<ChessMove> calculateValidMoves() {
-        return null;
+    /**
+     * Calculate and return the valid moves for the bishop
+     *
+     * @return Set of valid moves
+     */
+    public Set<ChessMove> calculateValidMoves() {
+        validMoves.clear(); // Clear any previous valid moves
+
+        calculateNormalMoves();
+
+        return validMoves;
+    }
+
+    private void calculateNormalMoves() {
+        // Check up
+        calculateMovesInDirection(1, 0);
+
+        // Check right
+        calculateMovesInDirection(0, 1);
+
+        // Check left
+        calculateMovesInDirection(0, -1);
+
+        // Check down
+        calculateMovesInDirection(-1, 0);
+
+        // Check upper right diagonal
+        calculateMovesInDirection(1, 1);
+
+        // Check upper left diagonal
+        calculateMovesInDirection(1, -1);
+
+        // Check lower right diagonal
+        calculateMovesInDirection(-1, 1);
+
+        // Check lower left diagonal
+        calculateMovesInDirection(-1, -1);
+    }
+
+    private void calculateMovesInDirection(int rowDirection, int colDirection) {
+        int currentRow = position.getRow() + rowDirection;
+        int currentCol = position.getColumn() + colDirection;
+
+        while (board.isValidPosition(new ChessPosition(currentRow, currentCol))) {
+            ChessPiece pieceAtCurrentPosition = board.getPiece(new ChessPosition(currentRow, currentCol));
+
+            if (pieceAtCurrentPosition == null) {
+                // Empty cell, add the move
+                validMoves.add(new ChessMove(position, new ChessPosition(currentRow, currentCol), null));
+            } else {
+                // Occupied cell, check if the piece is an opponent's piece
+                if (pieceAtCurrentPosition.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                    validMoves.add(new ChessMove(position, new ChessPosition(currentRow, currentCol), null));
+                }
+                break; // Stop checking in this direction after encountering the first piece
+            }
+
+            // Move to the next cell in the normal direction
+            currentRow += rowDirection;
+            currentCol += colDirection;
+        }
+
     }
 }
-
--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -1,13 +1,69 @@
 package chess;
 
-import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RookMovesCalculator {
-    public RookMovesCalculator(ChessBoard board, ChessPosition myPosition) {
+
+    private ChessBoard board;
+    private ChessPosition position;
+    private Set<ChessMove> validMoves;
+
+    public RookMovesCalculator(ChessBoard board, ChessPosition position) {
+        this.board = board;
+        this.position = position;
+        this.validMoves = new HashSet<>();
     }
 
-    public Collection<ChessMove> calculateValidMoves() {
-        return null;
+    /**
+     * Calculate and return the valid moves for the bishop
+     *
+     * @return Set of valid moves
+     */
+    public Set<ChessMove> calculateValidMoves() {
+        validMoves.clear(); // Clear any previous valid moves
+
+        calculateNormalMoves();
+
+        return validMoves;
+    }
+
+    private void calculateNormalMoves() {
+        // Check up
+        calculateNormalMovesInDirection(1, 0);
+
+        // Check right
+        calculateNormalMovesInDirection(0, 1);
+
+        // Check left
+        calculateNormalMovesInDirection(0, -1);
+
+        // Check down
+        calculateNormalMovesInDirection(-1, 0);
+    }
+
+    private void calculateNormalMovesInDirection(int rowDirection, int colDirection) {
+        int currentRow = position.getRow() + rowDirection;
+        int currentCol = position.getColumn() + colDirection;
+
+        while (board.isValidPosition(new ChessPosition(currentRow, currentCol))) {
+            ChessPiece pieceAtCurrentPosition = board.getPiece(new ChessPosition(currentRow, currentCol));
+
+            if (pieceAtCurrentPosition == null) {
+                // Empty cell, add the move
+                validMoves.add(new ChessMove(position, new ChessPosition(currentRow, currentCol), null));
+            } else {
+                // Occupied cell, check if the piece is an opponent's piece
+                if (pieceAtCurrentPosition.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                    validMoves.add(new ChessMove(position, new ChessPosition(currentRow, currentCol), null));
+                }
+                break; // Stop checking in this direction after encountering the first piece
+            }
+
+            // Move to the next cell in the normal direction
+            currentRow += rowDirection;
+            currentCol += colDirection;
+        }
+
     }
 }
-
