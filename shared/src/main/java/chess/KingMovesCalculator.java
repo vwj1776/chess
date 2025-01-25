f3c0e18507217f05cbbd418b8641c80f45bc2acb
vwj1776 <vwj1776@gmail.com>
2025-01-24 23:08:08 -0700

    added king moves

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,13 +1,113 @@
 package chess;
 
-import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class KingMovesCalculator {
-    public KingMovesCalculator(ChessBoard board, ChessPosition myPosition) {
+    private ChessBoard board;
+    private ChessPosition position;
+    private Set<ChessMove> validMoves;
+    private Set<ChessMove> movesGoingToCheck;
+
+
+
+
+    // just a test
+    public KingMovesCalculator(ChessBoard board, ChessPosition position) {
+        this.board = board;
+        this.position = position;
+        this.validMoves = new HashSet<>();
+        this.movesGoingToCheck = new HashSet<>();
     }
 
-    public Collection<ChessMove> calculateValidMoves() {
-        return null;
+    /**
+     * Calculate and return the valid moves for the bishop
+     *
+     * @return Set of valid moves
+     */
+    public Set<ChessMove> calculateValidMoves() {
+        validMoves.clear(); // Clear any previous valid moves
+
+        calculateMovesGoingToCheck();
+
+        calculateMoves();
+
+        return validMoves;
     }
+
+    private void calculateMovesGoingToCheck() {
+        BishopMovesCalculator bishopMovesCalculator = new BishopMovesCalculator(board, position);
+//        KnightMovesCalculator knightMovesCalculator = new KnightMovesCalculator(board, position);
+//        PawnMovesCalculator pawnMovesCalculator = new PawnMovesCalculator(board, position);
+//        QueenMovesCalculator queenMovesCalculator = new QueenMovesCalculator(board, position);
+//        RookMovesCalculator rookMovesCalculator = new RookMovesCalculator(board, position);
+//        movesGoingToCheck.addAll(bishopMovesCalculator.calculateValidMoves());
+//        movesGoingToCheck.addAll(knightMovesCalculator.calculateValidMoves());
+//        movesGoingToCheck.addAll(pawnMovesCalculator.calculateValidMoves());
+//        movesGoingToCheck.addAll(queenMovesCalculator.calculateValidMoves());
+//        movesGoingToCheck.addAll(rookMovesCalculator.calculateValidMoves());
+    }
+
+    private void calculateMoves() {
+        // Check up
+        calculateMovesInDirection(1, 0);
+
+        // Check right
+        calculateMovesInDirection(0, 1);
+
+        // Check left
+        calculateMovesInDirection(0, -1);
+
+        // Check down
+        calculateMovesInDirection(-1, 0);
+
+        // Check upper right diagonal
+        calculateMovesInDirection(1, 1);
+
+        // Check upper left diagonal
+        calculateMovesInDirection(1, -1);
+
+        // Check lower right diagonal
+        calculateMovesInDirection(-1, 1);
+
+        // Check lower left diagonal
+        calculateMovesInDirection(-1, -1);
+    }
+
+    private void calculateMovesInDirection2(int rowDirection, int colDirection) {
+        ChessPiece pieceAtNextPosition = board.getPiece(new ChessPosition(rowDirection, colDirection));
+
+        if (pieceAtNextPosition == null && movesGoingToCheck.contains(new ChessPosition(rowDirection, colDirection))) {
+            // Empty cell, add the move
+            validMoves.add(new ChessMove(position, new ChessPosition(rowDirection, colDirection), null));
+        } else if(pieceAtNextPosition != null){
+            // Occupied cell, check if the piece is an opponent's piece
+            if ((pieceAtNextPosition.getTeamColor() != board.getPiece(position).getTeamColor()) && movesGoingToCheck.contains(new ChessPosition(rowDirection, colDirection))) {
+                validMoves.add(new ChessMove(position, new ChessPosition(rowDirection, colDirection), null));
+            }
+        }
+
+    }
+
+    private void calculateMovesInDirection(int rowDirection, int colDirection) {
+        int currentRow = position.getRow() + rowDirection;
+        int currentCol = position.getColumn() + colDirection;
+
+        ChessPosition currentPosition = new ChessPosition(currentRow, currentCol);
+
+        if (board.isValidPosition(currentPosition)) {
+            ChessPiece pieceAtCurrentPosition = board.getPiece(currentPosition);
+
+            if (pieceAtCurrentPosition == null && !movesGoingToCheck.contains(currentPosition)) {
+                // Empty cell, add the move
+                validMoves.add(new ChessMove(position, currentPosition, null));
+            } else if (pieceAtCurrentPosition != null) {
+                // Occupied cell, check if the piece is an opponent's piece
+                if (pieceAtCurrentPosition.getTeamColor() != board.getPiece(position).getTeamColor() && !movesGoingToCheck.contains(currentPosition)) {
+                    validMoves.add(new ChessMove(position, currentPosition, null));
+                }
+            }
+        }
+    }
+
 }
-
